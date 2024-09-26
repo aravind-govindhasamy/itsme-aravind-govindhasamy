5c07abaac97e3d2ffc67287440b493378a1a731c
Aravind APG <70104627+aravind-govindhasamy@users.noreply.github.com>
2024-09-26 13:33:34 +0530

    Update index.tsx

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,35 +1,25 @@
 import {
-  FaReact,
   FaNodeJs,
-  FaCss3,
+  FaPython,
+  FaDatabase,
+  FaCogs,
   FaHtml5,
+  FaCss3,
   FaGitAlt,
-  FaAngular,
-  FaVuejs,
-  FaAws,
-  FaDocker,
 } from "react-icons/fa";
-import { SiTypescript, SiNativescript, SiMui } from "react-icons/si";
-import { GrGraphQl } from "react-icons/gr";
-import { IoLogoJavascript } from "react-icons/io5";
-import { BsBootstrapFill } from "react-icons/bs";
-import { RiNextjsFill } from "react-icons/ri";
+import { SiDotnet, SiPostman } from "react-icons/si"; // Importing .NET and Postman icons
+import { GiNfc } from "react-icons/gi"; // Importing NFC icon
 
 export const skillSets = [
-  { id: 1, icon: <FaReact />, name: "React" },
-  { id: 2, icon: <FaAngular />, name: "Angular" },
-  { id: 3, icon: <FaVuejs />, name: "VueJs" },
-  { id: 4, icon: <SiTypescript />, name: "Typescript" },
-  { id: 5, icon: <FaNodeJs />, name: "Node.js" },
-  { id: 6, icon: <GrGraphQl />, name: "GraphQL" },
-  { id: 7, icon: <RiNextjsFill />, name: "NextJs" },
-  { id: 8, icon: <SiNativescript />, name: "React Native" },
-  { id: 9, icon: <FaCss3 />, name: "CSS3" },
-  { id: 10, icon: <FaHtml5 />, name: "HTML5" },
-  { id: 11, icon: <IoLogoJavascript />, name: "JavaScript" },
-  { id: 12, icon: <BsBootstrapFill />, name: "Bootstrap" },
-  { id: 13, icon: <SiMui />, name: "Material UI" },
-  { id: 14, icon: <FaAws />, name: "AWS" },
-  { id: 15, icon: <FaDocker />, name: "Docker" },
-  { id: 16, icon: <FaGitAlt />, name: "Git" },
+  { id: 1, icon: <FaNodeJs />, name: "Node.js" },
+  { id: 2, icon: <FaGitAlt />, name: "GitHub" },
+  { id: 3, icon: <FaHtml5 />, name: "HTML" },
+  { id: 4, icon: <FaCss3 />, name: "CSS" },
+  { id: 5, icon: <FaJava />, name: "JavaScript" }, // Use a suitable icon for JavaScript
+  { id: 6, icon: <SiDotnet />, name: ".NET" }, // Using .NET icon for C#
+  { id: 7, icon: <FaPython />, name: "Python" },
+  { id: 8, icon: <FaDatabase />, name: "SQL" },
+  { id: 9, icon: <SiPostman />, name: "API Development" }, // Using Postman icon
+  { id: 10, icon: <GiNfc />, name: "IoT RFID" }, // Using NFC icon for IoT RFID
+  { id: 11, icon: <FaDatabase />, name: "MySQL Stored Procedures" }, // Using MySQL database icon
 ];
